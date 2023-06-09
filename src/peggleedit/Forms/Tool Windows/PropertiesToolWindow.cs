bae25062f0ffc90865fdd6810c4ca65aa2c232db
Ted John <ted@brambles.org>
2023-06-09 19:49:45 +0100

    Persist properties window scroll position

--- src/peggleedit/Forms/Tool Windows/PropertiesToolWindow.cs
+++ src/peggleedit/Forms/Tool Windows/PropertiesToolWindow.cs
@@ -14,7 +14,10 @@
 // You should have received a copy of the GNU General Public License
 // along with PeggleEdit. If not, see <http://www.gnu.org/licenses/>.
 
+using System;
 using System.Drawing;
+using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using IntelOrca.PeggleEdit.Designer.Properties;
@@ -25,38 +28,70 @@ namespace IntelOrca.PeggleEdit.Designer
     [GuidAttribute("1A01E050-F11A-47C5-B62B-000000000002")]
     class PropertiesToolWindow : Form
     {
-        MainMDIForm mParent;
-        PropertyGrid mPropertyGrid;
+        private MainMDIForm _parent;
+        private PropertyGrid _propertyGrid;
+        private int _lastScroll;
 
         public PropertiesToolWindow(MainMDIForm parent)
         {
-            mParent = parent;
+            _parent = parent;
 
             this.Icon = Icon.FromHandle(Resources.properties_16.GetHicon());
 
             FormBorderStyle = FormBorderStyle.SizableToolWindow;
             Text = "Properties";
 
-            mPropertyGrid = new PropertyGrid();
-            mPropertyGrid.Dock = DockStyle.Fill;
+            _propertyGrid = new PropertyGrid();
+            _propertyGrid.Dock = DockStyle.Fill;
 
-            mPropertyGrid.PropertyValueChanged += new PropertyValueChangedEventHandler(mPropertyGrid_PropertyValueChanged);
+            _propertyGrid.PropertyValueChanged += new PropertyValueChangedEventHandler(mPropertyGrid_PropertyValueChanged);
 
-            Controls.Add(mPropertyGrid);
+            Controls.Add(_propertyGrid);
         }
 
         void mPropertyGrid_PropertyValueChanged(object s, PropertyValueChangedEventArgs e)
         {
-            if (mPropertyGrid.SelectedObjects.Length == 0)
+            if (_propertyGrid.SelectedObjects.Length == 0)
                 return;
 
-            mParent.GetFocusedLevelEditor().UpdateRedraw();
+            _parent.GetFocusedLevelEditor().UpdateRedraw();
         }
 
         public void UpdatePropertyGrid(LevelEntry[] objects)
         {
-            mPropertyGrid.SelectedObjects = objects;
-            mPropertyGrid.ExpandAllGridItems();
+            if (_propertyGrid.SelectedObjects.Length != 0)
+            {
+                _lastScroll = GetScrollPosition();
+            }
+
+            _propertyGrid.SelectedObjects = objects;
+            _propertyGrid.ExpandAllGridItems();
+
+            SetScrollPosition(_lastScroll);
+        }
+
+        private int GetScrollPosition()
+        {
+            var vScroll = GetScrollBar();
+            return vScroll?.Value ?? 0;
+        }
+
+        private void SetScrollPosition(int value)
+        {
+            var vScroll = GetScrollBar();
+            vScroll.Value = Math.Min(value, vScroll.Maximum);
+        }
+
+        private VScrollBar GetScrollBar()
+        {
+            var propGrid = _propertyGrid.Controls
+                .OfType<Control>()
+                .Where(ctl => ctl.AccessibilityObject.Role == AccessibleRole.Table)
+                .First();
+            var vScroll = propGrid.Controls
+                .OfType<VScrollBar>()
+                .FirstOrDefault();
+            return vScroll;
         }
     }
 }
